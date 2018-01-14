d97c7e039d4c3b2df8d72b4aa4c928af1f23c886
rozalski <anfx@mail.ru>
2018-01-14 22:35:03 +0300

    add sort mass

--- less5/main.cpp
+++ less5/main.cpp
@@ -1,22 +1,64 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-
+#define MAX 10
 int main()
 {
-    int mass[3][3];
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-           mass[i][j] = rand() % 100 + 1;
+    int mass[MAX][MAX];
+    for(int i=0; i<MAX; i++)
+    {
+        for(int j=0; j<MAX; j++)
+        {
+            mass[i][j] = rand() % 99 + 1;
         }
     }
     system("clear"); // очищаем консоль
     cout << "Получен масссив : " << endl;
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            cout << mass[i][j] << " ";
+    for(int i=0; i<MAX; i++)
+    {
+        for(int j=0; j<MAX; j++)
+        {
+            if(mass[i][j]<10)
+                cout << mass[i][j] << "    ";
+            else
+                cout << mass[i][j] << "   ";
         }
-        cout << endl;
+        cout << endl << endl;
+    }
+    cout << "Сортировка массива...." << endl;
+    bool sorted=false;
+    int temp;
+    while(!sorted)
+    {
+        sorted=true;
+        int i=0, j=0;
+        for(int inext=0; inext<MAX; inext++)
+            for(int jnext=(inext?0:1); jnext<MAX; jnext++)
+            {
+                if(mass[i][j]>mass[inext][jnext])
+                {
+                    temp=mass[i][j];
+                    mass[i][j]=mass[inext][jnext];
+                    mass[inext][jnext]=temp;
+                    sorted=false;
+                }
+                i=inext;
+                j=jnext;
+            }
+
+    }
+
+    cout << "Получен масссив : " << endl;
+    for(int i=0; i<MAX; i++)
+    {
+        for(int j=0; j<MAX; j++)
+        {
+            if(mass[i][j]<10)
+                cout << mass[i][j] << "    ";
+            else
+                cout << mass[i][j] << "   ";
+        }
+        cout << endl << endl;
     }
 
     return 0;
