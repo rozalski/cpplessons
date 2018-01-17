5794f6c1f6715af94df635843dee4a17b64e2643
rozalski <anfx@mail.ru>
2018-01-17 20:16:46 +0300

    change

---
+++ less8/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "curl/curl.h"
+
+//---------------------------------------------------------------------------
+int main()
+{
+    CURL *  curl_handle = curl_easy_init();
+    if(curl_handle)
+        {
+             // задаем  url адрес
+             curl_easy_setopt(curl_handle, CURLOPT_URL, "http://www.cyberforum.ru");
+             // выполняем запрос
+             CURLcode res = curl_easy_perform(curl_handle);
+                         // закрываем дескриптор curl
+             curl_easy_cleanup(curl_handle);
+        }
+
+    getchar();
+    return 0;
+}
