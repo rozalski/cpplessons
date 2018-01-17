f1a2ff3dcd4e5534f953171a4541eac02464294b
rozalski <anfx@mail.ru>
2018-01-17 20:38:29 +0300

    dinamic array pointer

--- less7/main.cpp
+++ less7/main.cpp
@@ -1,34 +1,30 @@
-#include <GL/glut.h>
+/*
+Динамическое создание массива
+с испльзованием указателя
+*/
 
-void display()
-{
-  // рисует три линии: зеленую, синюю и красную
-  glClear(GL_COLOR_BUFFER_BIT);
-  glBegin(GL_LINES);
-  glColor3f(1.0, 0.0, 0.0);
-  glVertex2f(0.25, 0.25);
-  glVertex2f(0.75, 0.75);
-  glColor3f(0.0, 1.0, 0.0);
-  glVertex2f(0.75, 0.25);
-  glVertex2f(0.25, 0.75);
-  glColor3f(0.0, 0.0, 1.0);
-  glVertex2f(0.50, 0.25);
-  glVertex2f(0.50, 0.75);
-  glEnd();
-  glFlush();
-}
+#include <iostream>
 
-int main(int argc, char **argv)
+using namespace std;
+
+int main()
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(240, 240);
-  glutInitWindowPosition(100, 740);
-  glutCreateWindow("Test");
-  glClearColor(1.0, 1.0, 1.0, 1.0);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
-  glutDisplayFunc(display);
-  glutMainLoop();
+    int count_mas;
+    cout << "Введите количество элементов массива: ";
+    cin >> count_mas;
+    cout << "Введите значения массива: "<< endl;
+    int* arr = new int[count_mas]; // выделение памяти под массив
+    for(int i = 0; i< count_mas; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Вы ввели следующие значения массива: " << endl;
+    for(int i = 0; i< count_mas; i++)
+    {
+        cout << arr[i] << "  ";
+    }
+    cout << endl;
+    delete [] arr; // освобождение памяти
+
+    return 0;
 }
